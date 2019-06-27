786e891f1dec2406a52c832dc68783eceba87bca
Andres Munoz Acosta <munoz.m@unimelb.edu.au>
2019-06-28 08:55:49 +1000

    Fixed bug that breaks table writing when the 'instances' column is a number

--- matilda.m
+++ matilda.m
@@ -40,6 +40,10 @@ isfeat = strncmpi(varlabels,'feature_',8);
 isalgo = strncmpi(varlabels,'algo_',5);
 issource = strcmpi(varlabels,'source');
 instlabels = Xbar{:,isname};
+if isnumeric(isntlabels)
+    instlabels = num2cell(instlabels);
+    instlabels = cellfun(@(x) num2str(x),instlabels,'UniformOutput',false);
+end
 if any(issource)
     S = categorical(Xbar{:,issource});
     sourcelabels = cellstr(unique(S));
